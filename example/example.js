1027a6ae4cb00dc81c34f5b76537d40b45d8a4ee
Jayesh Bidani <jayesh.bidani@gmail.com>
2018-07-25 10:48:24 +0530

    identation issue

--- example/example.js
+++ example/example.js
@@ -5,20 +5,19 @@ const main = require('../src/index');
 var tryScraper = () => {
     let scraperObject = new main();
     console.log(argv);
-    pagination_type = argv['pagination'] || "-1";
-    filename = argv['filename'] || "data.json";
-    url = argv['url'] || "-1";
-    type = argv['type'] || "-1";
+    paginationType = argv['pagination'] || '-1';
+    filename = argv['filename'] || 'data.json';
+    url = argv['url'] || '-1';
+    type = argv['type'] || '-1';
     maxPagesToScrap = argv['pagecount'] || 1;
-    if(pagination_type == -1 || url == -1|| type == -1){
-        console.log("Arguments Not Passed Completely")
-    }else if(pagination_type == 'infinite_scrolling'){
-        // console.log("Passed")
+    if(paginationType == -1 || url == -1|| type == -1){
+        console.log('Arguments Not Passed Completely')
+    }else if(paginationType == 'infinite_scrolling'){
         scraperObject.runInfiniteScrollPagination(url,filename);
-    }else if(pagination_type == 'page_wise'){    
+    }else if(paginationType == 'page_wise'){    
         scraperObject.runPageWisePagination(url,filename,maxPagesToScrap);
     }else{
-        console.log("Some Error/No Match Found");
+        console.log('Some Error/No Match Found');
     }
 }
 
--- src/scraper.js
+++ src/scraper.js
@@ -20,7 +20,7 @@ class scraper {
                     dataJson.rating = element.querySelector('span.review-score-badge').innerText;
                 }
                 catch (exception){
-                    console.dir(" Exception Occured ");
+                    console.dir(' Exception Occured ');
                     console.dir(exception);
                 }
                 scrapedData.push(dataJson);
